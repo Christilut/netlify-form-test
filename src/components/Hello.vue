d499a300cf70d1599f462a7e8d8223c685ad793a
Christiaan Maks <c.maks@innovadis.com>
2016-11-16 16:31:48 +0100

    f

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -25,10 +25,10 @@
     li
       a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
   h4 This form is nested and created with Jade/Pug, maybe that matters
-  form
+  form(action='/', name='contact')
     input(type='hidden' name='form-name' value='contact')
     input(name='somethingelse')
-    button(type='submit')
+    button(type='submit') submit
 </template>
 
 <script>
